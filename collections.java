a90ca89676a1edff3be6cf66c02bed4edbc37612
RenukaSrivalli <renuka1257@sasi.ac.in>
2022-07-20 20:14:06 +0530

    coll

---
+++ collections.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Collections
+{
+	public static void main(String args[])
+	{
+		List<Integer> l=new LinkedList<>();
+		l.add(1);
+		l.add(2);
+		System.out.println(l);
+	}
+}
