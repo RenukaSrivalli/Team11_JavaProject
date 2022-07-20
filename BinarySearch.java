87ea2608fffd02b4cc6afb6ffcad575b94d4b368
RenukaSrivalli <renuka1257@sasi.ac.in>
2022-07-20 18:57:45 +0530

    binary

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,7 +4,7 @@ class BinarySearch
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter array size : ");
+System.out.println("Enter the size of array : ");
 int size=sc.nextInt();
 int arr[]=new int[size];
 System.out.println("Enter array elements : ");
