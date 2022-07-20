33b9151c94251e1c5b4b6afbb97c5f845cdf760d
RenukaSrivalli <renuka1257@sasi.ac.in>
2022-07-20 20:48:28 +0530

    arith

--- arithmetic.java
+++ arithmetic.java
@@ -4,11 +4,11 @@ public static void main(String args[])
 {
 int a=10;
 int b=20;
-int d=a-b;
+int c=a-b;
 int e=a*b;
 int f=a/b;
 System.out.println("The addition of a and b is "+a+b);
-System.out.println("The subtraction of a and b is "+ d);
+System.out.println("The subtraction of a and b is "+ c);
 System.out.println("The multiplication of a and b is "+ e);
 System.out.println("The division of a and b is "+ f);
 }
