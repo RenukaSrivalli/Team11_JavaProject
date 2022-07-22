fcc5a2f311c9922ae5473fa930ec898f1fd12687
RenukaSrivalli <renuka1257@sasi.ac.in>
2022-07-22 22:27:21 +0530

    arm

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Palindrome
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number");
+int num=sc.nextInt();
+int n;
+n=num;
+int rem;
+int rev=0;
+while(num!=0)
+{
+rem=num%10;
+rev=(rev*10)+rem;
+num=num/10;
+}
+System.out.println("Reverse of number "+n+" is "+rev);
+if(n==rev)
+{
+System.out.println("Armstrong number");
+}
+else
+{
+System.out.println("NOt a Armstrong number");
+}
+}
+}
